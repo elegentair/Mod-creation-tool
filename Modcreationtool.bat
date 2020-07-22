4f553045413c200c661e2aea02bfda5ca2c92f1d
elegentair <49598332+elegentair@users.noreply.github.com>
2020-07-22 17:01:46 -0400

    Update Modcreationtool.bat

--- Modcreationtool.bat
+++ Modcreationtool.bat
@@ -1,1 +1,34 @@
+@echo off
+color 1f
+title mod creation tool
+echo welcome to the mod creation tool
+echo download the latest release end of the world text game and right click it than press edit.
+pause
+cls
+echo now make sure you know what all the commands in the game code are, they are batch
+pause
+cls
+echo now you can add content to your hearts content
+pause
+cls
+echo make sure to list this as a end of the world text game mod
+echo DO NOT LIST IT AS A WHOLE NEW GAME or it could be taken down from github 
+pause
+cls
+echo here are some examples of what you can NOT call your mod, MY 1st game, my exiting new game ect
+pause
+cls
+echo it must have this statement in the description "this is a mod for the end of the world text game, this is not a new game"
+pause
+cls
+echo if you do not follow these things when making a mod for this game then your mod could be taken down
+pause
+cls
+echo thank you for making mods for this game.
+pause 
+cls
+echo bye
+pause
+cls
+
 
